b6e2c0f1d5577c74da83cbc42dd61490febd96e0
Fernando Zambone <fernando.zambone@gmail.com>
2025-04-17 19:50:00 -0300

    Feat(core): Implement SoftDelete base model and manager

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+    def archives(self):
+        return super().get_queryset().filter(deleted_at__isnull=False)
+
+    def all_including_deleted(self):
+        return super().get_queryset()
+
+
+class SoftDeleteModel(models.Model):
+    deleted_at = models.DateTimeField(null=True, black=True, default=None)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
+
+    class Meta:
+        abstract = True
